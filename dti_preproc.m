fc3fe4d41a0cbc8f4162f6f5aff364a6885ad41e
Gio <github@gpiantoni.com>
2012-04-02 15:27:04 +0200

    added processing of FA

--- dti_preproc.m
+++ dti_preproc.m
@@ -8,19 +8,14 @@ function dti_preproc(cfg, subj)
 %  .dti.mod: 'smri' (modality of DTI)
 %  .dti.cond: 'dti' (condition of DTI)
 %
-%  .dti.ec: run eddy currect correction or not (logical)
 %  .dti.b0: index of the volume with no gradient applied (use FSL convetion starting at 0)
 %  
 % INPUT
 %  Should be in CFG.DATA/0001/CFG.DTI.MOD/CFG.DTI.COND/ and contain:
 %  - PROJNAME_SUBJ_smri_dti.nii.gz: diffusion-weighted images
-%  - PROJNAME_SUBJ_smri_dti.bval: b-values (vector 0 and 1000 usually)
-%  - PROJNAME_SUBJ_smri_dti_orig.bvec: b-vectors calculated from PAR file
-%  - PROJNAME_SUBJ_smri_dti_orig.grad: b-vectors calculated from the GRAD file
-%  you can specify which b-vector file you want to use with cfg.bvec
 %  - PROJNAME_SUBJ_smri_magn.nii.gz: magnitude information of fieldmaps (optional)
 %  - PROJNAME_SUBJ_smri_phase.nii.gz: phase information of fieldmaps (optional)
-% 
+%
 % OUTPUT
 %  - PROJNAME_SUBJ_smri_dti_ng.nii.gz: b0 image from DWI
 %  - PROJNAME_SUBJ_smri_dti_ng_mask.nii.gz: mask for DWI
@@ -39,9 +34,6 @@ tic_t = tic;
 %---------------------------%
 %-dir and files
 ddir = sprintf('%s1%03.f/%s/%s/', cfg.data, subj, cfg.dti.mod, cfg.dti.cond); % data directory
-fadir = [ddir 'fa/']; % FA directory
-if isdir(fadir); rmdir(fadir, 's'); end
-mkdir(fadir)
 
 dfile = sprintf('%s_%04.f_%s_%s', cfg.rec, subj, cfg.dti.mod, cfg.dti.cond); % data
 ngfile = [dfile '_ng']; % Not-gradient
@@ -64,7 +56,7 @@ system(['bet ' ddir ngfile ' ' ddir ngfile ' -m -f .3']); % change -f, (no -n be
 
 %-----------------%
 %-eddy current correction
-system(['eddy_correct ' ddir dfile ' ' ddir ecfile ' ' num2str(cfg.dti.b0)]);
+% system(['eddy_correct ' ddir dfile ' ' ddir ecfile ' ' num2str(cfg.dti.b0)]);
 %-----------------%
 %---------------------------%
 
