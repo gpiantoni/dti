72af85c8de3a42eba1902b777b30e05b5a04bbf1
Gio <github@gpiantoni.com>
2012-04-13 14:30:48 +0200

    added functions to create design matrix and run randomise

---
+++ dti_design.m
@@ -0,0 +1,198 @@
+function dti_design(cfg)
+%DTI_DESIGN create design matrix for TBSS analysis
+%
+% CFG
+%  .dtifa.tbss: directory name for all FA etc files
+% 
+%  .tbss.des: one or more structures, with fields:
+%         .name: name of the contrast
+%         .fun: name of the function (see below)
+%         .corr: 1 (for positive correlation) or -1 for negative
+%                correlation (one value per column of the design matrix) 
+%         .demean: if your design columns should be demeaned (logical, default is TRUE)
+%         .ones: if it adds a column of ones to your design matrix (logical, default is TRUE)
+%  The function gets as input the cfg and the index of cfg.tbss.des in the
+%  loop. It should return a design matrix. The size of the design matrix is
+%  NxC, where N is the number of subjects and C is the number of regressors/contrasts.   
+%  For example, you can return a random design matrix with two regressors:
+%      function [des] = des_random(cfg, d)
+%      des = randn(numel(cfg.subjall), 2);
+%  It's very important that N is equal to numel(cfg.subjall). If you don't
+%  analyze some subjects, you should be careful to do: 
+%    for i = 1:numel(cfg.subjall)
+%      des(i, 1) = single subject value
+%    end
+%
+% INPUT
+%  Depend on your own function
+%
+% OUTPUT
+%  cfg.dtifa.tbss directory is ready for statistics (DTI_RAND)
+%
+% Part of DTI
+% see also DTI_CONVERT, DTI_PREPROC, DTI_FA, DTI_BEDPOSTX
+%          DTI_TBSS, DTI_DESIGN, DTI_RAND
+
+%---------------------------%
+%-start log
+output = sprintf('%s started at %s on %s\n', ...
+  mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
+tic_t = tic;
+%---------------------------%
+
+%---------------------------%
+%-directory with design
+desd = [cfg.dtifa.tbss 'design/'];
+if isdir(desd); rmdir(desd, 's'); end
+mkdir(desd)
+%---------------------------%
+
+%---------------------------%
+%-check if two designs have the same name
+if numel(unique({cfg.tbss.des.name})) ~= numel(cfg.tbss.des)
+  output = sprintf('%sWARNING: Some of your designs (cfg.tbss.des) have the same name. The first design will be overwritten\n', output);
+end
+%---------------------------%
+
+%-------------------------------------%
+%-loop over designs
+for d = 1:numel(cfg.tbss.des)
+  
+  %---------------------------%
+  %-dir and files
+  desfile = [desd cfg.tbss.des(d).name];
+  output = [output sprintf('\nContrast: %s\n', cfg.tbss.des(d).name)];
+  
+  if ~isfield(cfg.tbss.des(d), 'demean') || isempty(cfg.tbss.des(d).demean)
+    cfg.tbss.des(d).demean = true; 
+  end
+  if ~isfield(cfg.tbss.des(d), 'ones') || isempty(cfg.tbss.des(d).ones)
+    cfg.tbss.des(d).ones = true; 
+  end
+  %---------------------------%
+  
+  %---------------------------%
+  %-run specific function to prepare design
+  des = feval(cfg.tbss.des(d).fun, cfg, d);
+  
+  %-----------------%
+  %-check input and return values
+  if size(des,2) ~= numel(cfg.tbss.des(d).corr)
+    output = sprintf('%sWARNING: number of design matrix columns (%d) is different from the number of contrasts(%d)\nSkipping the contrast\n', ...
+      output, size(des,2), numel(cfg.tbss.des(d).corr));
+    
+    continue
+  end
+  
+  output = [output sprintf('Non-demeaned values\n')];
+  for i = 1:size(des,2)
+    desstr = sprintf('%10.3f', des(:,i));
+    output = [output sprintf('contrast #%3d (%2d): %s\n', i, cfg.tbss.des(d).corr(i), desstr)];
+  end
+  %-----------------%
+  
+  %-----------------%
+  %-demean
+  if cfg.tbss.des(d).demean
+    des = des - repmat(mean(des), numel(cfg.subjall), 1);
+  end
+  %-----------------%
+  
+  %-----------------%
+  %-show correlation between columns
+  des_corr = corrcoef(des);
+  output = [output sprintf('Correlation between regressors\n')];
+  for i = 1:size(des_corr,1)
+    output = [output sprintf('%6.2f', des_corr(i,:)) sprintf('\n')];
+  end
+  %-----------------%
+  
+  %-----------------%
+  %-ones
+  descon = des; % we use this later to describe the contrasts
+  if cfg.tbss.des(d).ones
+    des = [ones(size(des,1),1) des];
+  end
+  %-----------------%
+  %---------------------------%
+  
+  %---------------------------%
+  %-write design in FSL format
+  %-----------------%
+  %-write mat
+  fid = fopen([desfile '.mat'], 'w');
+  fwrite(fid, sprintf('/NumWaves\t%1.f\n', size(des,2)));
+  fwrite(fid, sprintf('/NumPoints\t%1.f\n', size(des,1)));
+  
+  fwrite(fid, sprintf('/PPheights\t'));
+  for c = 1:size(des,2)
+    fwrite(fid, sprintf('\t%1.5f', range(des(:,c))));
+  end
+  fwrite(fid, sprintf('\n\n'));
+  
+  fwrite(fid, sprintf('/Matrix\n'));
+  for s = 1:size(des,1)
+    fwrite(fid, sprintf('%1.5f ', des(s,:)));
+    fwrite(fid, sprintf('\n'));
+  end
+  
+  fclose(fid);
+  %-----------------%
+  
+  %-----------------%
+  %-contrast
+  %-------%
+  %-write con
+  fid = fopen([desfile '.con'], 'w');
+  for c = 1:size(descon,2)
+    fwrite(fid, sprintf('/ContrastName%1.f\t"con%1.f"\n', c, c));
+  end
+  
+  fwrite(fid, sprintf('/NumWaves\t%1.f\n', size(des,2)));
+  fwrite(fid, sprintf('/NumContrasts\t%1.f\n', size(descon,2)));
+  
+  fwrite(fid, sprintf('/PPheights\t'));
+  for c = 1:size(descon,2)
+    fwrite(fid, sprintf('\t%1.5f', range(descon(:,c))));
+  end
+  fwrite(fid, sprintf('\n'));
+  
+  printones = sprintf('%1.f ', ones(size(descon,2),1));
+  fwrite(fid, sprintf('/RequiredEffect\t\t%s\n\n', printones));
+  
+  fwrite(fid, sprintf('/Matrix\n'));
+  
+  coneye = diag(cfg.tbss.des(d).corr);
+  
+  for c = 1:size(descon,2)
+    
+    if cfg.tbss.des(d).ones
+      printcon = ['0 ' sprintf('%1.f ', coneye(c,:))];
+    else
+      printcon = sprintf('%1.f ', coneye(c,:));
+    end
+    
+    fwrite(fid, sprintf('%s\n', printcon));
+  end
+  fclose(fid);
+  %-----------------%
+  %---------------------------%
+  
+end
+%-------------------------------------%
+
+%---------------------------%
+%-end log
+toc_t = toc(tic_t);
+outtmp = sprintf('%s ended at %s on %s after %s\n\n', ...
+  mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
+  datestr( datenum(0, 0, 0, 0, 0, toc_t), 'HH:MM:SS'));
+output = [output outtmp];
+
+%-----------------%
+fprintf(output)
+fid = fopen([cfg.log '.txt'], 'a');
+fwrite(fid, output);
+fclose(fid);
+%-----------------%
+%---------------------------%
---
+++ dti_rand.m
@@ -0,0 +1,151 @@
+function dti_rand(cfg)
+%DTI_RAND run randomise on the folder
+%
+% CFG
+%  .dtifa.tbss: directory name for all FA etc files
+%  .dtifa.type: type of images to analyze (a cell, as {'FA' 'L1' 'RD'})
+%
+%  .dtirand: string with options to pass to randomise:
+%    ' -D ': to demean the DTI values (suggested, especially if you're not
+%    using the intercept in design matrix)
+%    ' -n XXXX ': number of randomizations
+%    ' --T2 ': TFCE correction (best options). All options are
+%  FWE:
+%    -x   -> single voxel correction: _vox_p_tstat  | _vox_corrp_tstat
+%    --T2 -> TFCE correction:         _tfce_p_tstat | _tfce_corrp_tstat
+%    -c N -> cluster-size correction:               | _clustere_corrp_tstat
+%    -C N -> cluster-mass correction:               | _clusterm_corrp_tstat
+%
+% INPUT
+%  cfg.dtifa.tbss directory, with subfolder:
+%   1) 'design', with files .con and .mat, the design matrix (from DTI_DESIGN)
+%   2) 'stats', created by FSL (from DTI_TBSS)
+%
+% OUTPUT
+%  cfg.dtifa.tbss has stats analyzed
+%
+% Part of DTI
+% see also DTI_CONVERT, DTI_PREPROC, DTI_FA, DTI_BEDPOSTX
+%          DTI_TBSS, DTI_DESIGN, DTI_RAND
+
+%---------------------------%
+%-start log
+output = sprintf('%s started at %s on %s\n', ...
+  mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'));
+tic_t = tic;
+%---------------------------%
+
+%---------------------------%
+%-directory with design and for randomisation
+desd = [cfg.dtifa.tbss 'design/'];
+randd = [cfg.dtifa.tbss 'rand/'];
+if isdir(randd); rmdir(randd, 's'); end
+mkdir(randd)
+%---------------------------%
+
+%-------------------------------------%
+%-loop over designs
+%---------------------------%
+%-check which designs are available in design/ folder (don't rely on
+%DTI_DESIGN, some of them might not be available)
+des = dir([desd '*.con']);
+%---------------------------%
+
+nimg = 0;
+for d = 1:numel(des)
+  
+  %---------------------------%
+  %-dir and files
+  desname = des(d).name(1:end-4);
+  desmat = [desd desname '.mat'];
+  descon = [desd desname '.con'];
+  %---------------------------%
+  
+  %---------------------------%
+  %-count number of contrasts in one design
+  fid = fopen(descon, 'r');
+  ncon = 0;
+  while 1
+    
+    l = fgetl(fid);
+    if numel(l) < 13 || ~strcmp(l(1:13), '/ContrastName')
+      break
+    end
+    ncon = ncon + 1;
+  end
+  fclose(fid);
+  %---------------------------%
+  
+  %---------------------------%
+  %-----------------%
+  %-compute model
+  cdir = pwd;
+  cd(cfg.dtifa.tbss)
+  
+  %-------%
+  %-parameter for randomise
+  opt = [];
+  opt.d = desmat;
+  opt.t = descon;
+  opt.m = [cfg.dtifa.tbss 'stats/mean_FA_skeleton_mask.nii.gz'];
+  %-------%
+  
+  for i = 1:numel(cfg.dtifa.type)
+    if ~strcmp(cfg.dtifa.type{i}(1), 'V') % it cannot handle 3d data
+      
+      nimg = nimg + ncon; % corrp_tstat1, corrp_tstat2, corrp_tstat3 etc
+      
+      opt.i = [cfg.dtifa.tbss 'stats/all_' cfg.dtifa.type{i} '.nii.gz'];
+      opt.o = [randd desname '_' cfg.dtifa.type{i}];
+      system(['randomise_parallel -i ' opt.i ' -o "' opt.o '" -d "' opt.d '" -t "' opt.t '" -m ' opt.m ' ' cfg.dtirand]);
+      
+    end
+  end
+  
+  cd(cdir)
+  %-----------------%
+  %---------------------------%
+  
+end
+
+%-----------------%
+%-check whether the program has finished
+[~, username] = system('whoami');
+username(end) = [];
+while 1
+  pause(15)
+  
+  %-method 1: check if randomise running
+  %it fails bc it takes some time to initialize
+  [~, running] = system(['ps -u ' username ' | grep -c randomise']);
+  fprintf([datestr(now, 'HH:MM:SS') '  ' running])
+  
+  %-method 2:
+  allwr = dir([randd '*_corrp_tstat*.nii.gz']);
+  allseed = numel(find(~cellfun(@isempty, strfind({allwr.name}, 'SEED'))));
+
+  if numel(allwr) - allseed == nimg
+    break
+  end
+  
+  pause(45)
+end
+disp('done')
+%-----------------%
+%-------------------------------------%
+
+%---------------------------%
+%-end log
+toc_t = toc(tic_t);
+outtmp = sprintf('%s ended at %s on %s after %s\n\n', ...
+  mfilename, datestr(now, 'HH:MM:SS'), datestr(now, 'dd-mmm-yy'), ...
+  datestr( datenum(0, 0, 0, 0, 0, toc_t), 'HH:MM:SS'));
+output = [output outtmp];
+
+%-----------------%
+fprintf(output)
+fid = fopen([cfg.log '.txt'], 'a');
+fwrite(fid, output);
+fclose(fid);
+%-----------------%
+%---------------------------%
